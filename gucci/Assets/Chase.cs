1182253f56e69479dafe43e9d309260ada476389
Oliver <oliver.malmivaara@gmail.com>
2020-10-08 22:57:50 +0300

    Masit seuraa

---
+++ gucci/Assets/Chase.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Chase : MonoBehaviour
+{
+    NavMeshAgent nm;
+    public GameObject Player;
+
+
+
+    void Start()
+    {
+        nm = GetComponent<NavMeshAgent>();
+        Player = GameObject.FindWithTag("Player");
+    }
+
+    void Update()
+    {
+        nm.SetDestination(Player.transform.position);
+    }
+}
