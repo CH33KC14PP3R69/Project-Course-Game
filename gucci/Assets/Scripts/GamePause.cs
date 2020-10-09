ec0581f4b4fc7d0e364042fd99fedf4eae66152c
Leevi <leevi.frimodig.02@gmail.com>
2020-10-09 12:35:34 +0300

    Pause Game
    pelin pystyy pausettaan

---
+++ gucci/Assets/Scripts/GamePause.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePause : MonoBehaviour
+{
+    public bool gamePaused = false;
+    public GameObject pauseMenu;
+    private static bool cursorLocked = true;
+    void Start()
+    {
+        Time.timeScale = 1;
+    }
+    void Update()
+    {
+        if (Input.GetButtonUp("Cancel"))
+            if (gamePaused == false)
+            {
+                Time.timeScale = 0;
+                gamePaused = true;
+                //Cursor.visible = true;
+                pauseMenu.SetActive(true);
+            }
+            else
+            {
+                pauseMenu.SetActive(false);
+                //Cursor.visible = false;
+                gamePaused = false;
+                Time.timeScale = 1;
+        }
+    }
+}
